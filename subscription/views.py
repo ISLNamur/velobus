245b9cc3e091a64184ef62788dacc60d519dbda9
Supermanu <manueltondeur@gmail.com>
2022-08-31 11:22:06 +0200

    Fix welcoming email text

--- subscription/views.py
+++ subscription/views.py
@@ -1,24 +1,29 @@
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
 from django.core.mail import send_mail
+from django.conf import settings
 
 from . import models, serializers
 
 
 def send_welcome_mail(person: str, uuid: str, recipient: str) -> int:
     subject = "Inscription vélobus"
-    domain = ""
+    base_url = (
+        f"https://{settings.ALLOWED_HOSTS[0]}"
+        if settings.ALLOWED_HOSTS
+        else "http://localhost"
+    )
     raw_message = (
         """Bonjour,
-        Merci pour votre inscription. Retrouvez votre toutes les informations concernant votre inscription à l'adresse suivante:
-        https://"""
-        + domain
+        Merci pour votre inscription. Retrouvez toutes les informations concernant votre inscription à l'adresse suivante:
+        """
+        + base_url
         + f"/#/{person}/1/{uuid}"
         + """
         L'équipe Vélobus
         """
     )
-    html_message = f"<p>Bonjour</p><p>Merci pour votre inscription. Retrouvez votre toutes les informations concernant votre inscription par le lien suivant: <a href='https://{domain}/#/{person}/1/{uuid}'>inscription</a>.</p><p>Cordialement<br>L'équipe Vélobus</p>"
+    html_message = f"<p>Bonjour</p><p>Merci pour votre inscription. Retrouvez toutes les informations concernant votre inscription par le lien suivant: <a href='{base_url}/#/{person}/1/{uuid}'>inscription</a>.</p><p>Cordialement<br>L'équipe Vélobus</p>"
 
     return send_mail(
         subject=subject,
